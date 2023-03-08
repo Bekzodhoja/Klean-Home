e4a176f6a6666a5e8375bf8e58993d1e2d82c7d0
Bekzodxoja <100826293+bekzodxoja@users.noreply.github.com>
2023-03-08 11:04:24 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[PageController::class,'main']);
+Route::get('/about',[PageController::class,'about'])->name('about');
+Route::get('/service',[PageController::class,'service'])->name('service');
+Route::get('/project',[PageController::class,'project'])->name('project');
+
+
+
+
+
+
+
+// Route::get('user', [UserController::class, 'index']);
+// Route::get('main/{id?}', [UserController::class, 'show']);
+// Route::get('create', [UserController::class, 'create']);
+// Route::post('/user-create', [UserController::class, 'store']);
